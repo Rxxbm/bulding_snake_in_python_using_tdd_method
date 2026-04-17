eac6346c161957d7fd86f0bd5632a150e90ee671
Rubem <rubemcorrea0@gmail.com>
2026-04-17 18:07:01 -0300

    fix: retirando '-' não reconhecido em filename

---
+++ snake_screen.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import tty
+import termios
+import threading
+import time
+
+
+class io_handler:
+
+    x_size: int
+    y_size: int
+    game_speed: float
+    last_input: str
+
+    def __init__(self, dim, speed):
+        self.x_size = dim[0]
+        self.y_size = dim[1]
+        self.game_speed = speed
+        self.last_input = 'w'
+        self.matrix = [[0] * self.x_size for _ in range(self.y_size)]
+
+    def record_inputs(self):
+        def read_keys():
+            fd = sys.stdin.fileno()
+            old = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                while True:
+                    ch = sys.stdin.read(1)
+                    if ch in ('w', 'a', 's', 'd'):
+                        self.last_input = ch
+                    elif ch == '\x1b':
+                        self.last_input = 'end'
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old)
+
+        t = threading.Thread(target=read_keys, daemon=True)
+        t.start()
+
+    def display(self):
+        def display_h_line(self):
+            print ('+', end='')
+            print ('--'* len(self.matrix[0]), end='')
+            print ('+')
+        
+        def display_content_line(line):
+            print ('|', end='')
+            for item in line: 
+                if item == 1:
+                    print ('[]', end='')
+                elif item == 2:
+                    print ('<>', end='')
+                elif item == 3:
+                    print ('()', end='')
+                else:
+                    print ('  ', end='')
+
+            print ('|')
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        display_h_line(self)
+        for line in self.matrix:
+            display_content_line(line)
+        display_h_line(self)
+
+### exemplo do uso da classe io_handler
+if __name__ == '__main__':
+    instance = io_handler((10,15), 0.5)
+    instance.matrix[0][0] = 1 #corpo
+    instance.matrix[0][1] = 2 #cabeça
+    instance.matrix[0][2] = 3 #fruta
+
+    def game_loop():
+        instance.record_inputs()
+        while True:
+            instance.display()
+            print("mova com WASD, saia com esc. Ultimo botão:", end=' ')
+            ###adicione seu código para lidar com o jogo aqui
+
+            print(instance.last_input)
+            if(instance.last_input == 'end'):
+                exit()
+            time.sleep(instance.game_speed)
+
+    game_loop()
