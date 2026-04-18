ab151264518ded89d2e4a1be2b87429e7de91e6f
Rubem <rubemcorrea0@gmail.com>
2026-04-18 15:40:45 -0300

    feat: implementando renderização estável e lógica de múltiplas frutas

--- snake_screen.py
+++ snake_screen.py
@@ -63,16 +63,11 @@ class Snake:
             self._body.pop()
 
 class io_handler:
-    x_size: int
-    y_size: int
-    game_speed: float
-    last_input: str
-
     def __init__(self, dim, speed):
         self.x_size = dim[0]
         self.y_size = dim[1]
         self.game_speed = speed
-        self.last_input = 'w'
+        self.last_input = 'd'
         self.matrix = [[0] * self.x_size for _ in range(self.y_size)]
 
     def record_inputs(self):
@@ -85,57 +80,96 @@ class io_handler:
                     ch = sys.stdin.read(1)
                     if ch in ('w', 'a', 's', 'd'):
                         self.last_input = ch
-                    elif ch == '\x1b':
+                    elif ch == '\x1b': # ESC
                         self.last_input = 'end'
+                        break
             finally:
                 termios.tcsetattr(fd, termios.TCSADRAIN, old)
-
         t = threading.Thread(target=read_keys, daemon=True)
         t.start()
 
-    def display(self):
-        def display_h_line(self):
-            print ('+', end='')
-            print ('--'* len(self.matrix[0]), end='')
-            print ('+')
-        
-        def display_content_line(line):
-            print ('|', end='')
-            for item in line: 
-                if item == 1:
-                    print ('[]', end='')
-                elif item == 2:
-                    print ('<>', end='')
-                elif item == 3:
-                    print ('()', end='')
-                else:
-                    print ('  ', end='')
+    def update_state(self, snake, fruits):
+        for r in range(self.y_size):
+            for c in range(self.x_size):
+                self.matrix[r][c] = 0
+        for fr, fc in fruits:
+            if 0 <= fr < self.y_size and 0 <= fc < self.x_size:
+                self.matrix[fr][fc] = 3
+        for i, (r, c) in enumerate(snake.body()):
+            if 0 <= r < self.y_size and 0 <= c < self.x_size:
+                self.matrix[r][c] = 2 if i == 0 else 1
 
-            print ('|')
-
-        os.system('cls' if os.name == 'nt' else 'clear')
-        display_h_line(self)
+    def display(self, snake, fruits):
+        output = "\033[H" 
+        h_line = "+" + "--" * self.x_size + "+\r\n"
+        output += h_line
         for line in self.matrix:
-            display_content_line(line)
-        display_h_line(self)
+            row_str = "|"
+            for item in line:
+                if item == 1: row_str += "[]"
+                elif item == 2: row_str += "<>"
+                elif item == 3: row_str += "()"
+                else: row_str += "  "
+            row_str += "|\r\n"
+            output += row_str
+        output += h_line
+        score = len(snake.body()) - 1
+        output += f"TECLA: {self.last_input.upper()} | SCORE: {score} | FRUTAS: {len(fruits)}\r\n"
+        output += "WASD para mover | ESC para sair\r\n"
+        sys.stdout.write(output)
+        sys.stdout.flush()
 
-### exemplo do uso da classe io_handler
-if __name__ == '__main__':
-    instance = io_handler((10,15), 0.5)
-    instance.matrix[0][0] = 1 #corpo
-    instance.matrix[0][1] = 2 #cabeça
-    instance.matrix[0][2] = 3 #fruta
+def get_fruit_spawn_pos(snake, fruits, bounds):
+    while True:
+        r = random.randint(0, bounds[1] - 1)
+        c = random.randint(0, bounds[0] - 1)
+        if (r, c) not in snake.body() and (r, c) not in fruits:
+            return (r, c)
 
-    def game_loop():
-        instance.record_inputs()
+def main():
+    DIM = (20, 10) 
+    SPEED = 0.15
+    
+    screen = io_handler(DIM, SPEED)
+    start_pos = (DIM[1] // 2, DIM[0] // 2)
+    snake = Snake(start=start_pos, bounds=DIM, wrap=True)
+    fruits = [get_fruit_spawn_pos(snake, [], DIM)]
+
+    screen.record_inputs()
+    sys.stdout.write("\033[2J")
+    
+    try:
         while True:
-            instance.display()
-            print("mova com WASD, saia com esc. Ultimo botão:", end=' ')
-            ###adicione seu código para lidar com o jogo aqui
+            current_input = screen.last_input
+            if current_input == 'end': break
 
-            print(instance.last_input)
-            if(instance.last_input == 'end'):
-                exit()
-            time.sleep(instance.game_speed)
+            snake.move(current_input)
+            if snake.is_dead():
+                screen.update_state(snake, fruits)
+                screen.display(snake, fruits)
+                sys.stdout.write("\r\n[ GAME OVER ]\r\n")
+                break
 
-    game_loop()
+            # Verifica se comeu QUALQUER uma das frutas
+            head = snake.head()
+            if head in fruits:
+                snake.grow()
+                fruits.remove(head)
+            
+            # Lógica de spawn de frutas: quantidade baseada em (tamanho // 10) + 1
+            required_fruits = (len(snake.body()) // 10) + 1
+            while len(fruits) < required_fruits:
+                fruits.append(get_fruit_spawn_pos(snake, fruits, DIM))
+
+            screen.update_state(snake, fruits)
+            screen.display(snake, fruits)
+            time.sleep(screen.game_speed)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sys.stdout.write("\033[?25h")
+        sys.stdout.flush()
+
+if __name__ == '__main__':
+    sys.stdout.write("\033[?25l")
+    main()
