3bb045bcf2a855e4809173202012fbc1491265d6
Rubem <rubemcorrea0@gmail.com>
2026-04-18 15:40:42 -0300

    feat: implementando lógica de wrap e colisão de corpo na classe Snake

--- snake_screen.py
+++ snake_screen.py
@@ -4,14 +4,19 @@ import tty
 import termios
 import threading
 import time
+import random
 
 class Snake:
-    def __init__(self, start, bounds=(10,10)):
-        self._body = [start]
+    def __init__(self, start, bounds=(10,10), wrap=False):
+        if isinstance(start, list):
+            self._body = list(start)
+        else:
+            self._body = [start]
         self.grow_pending = 0
         self.current_dir = ''
         self.dead = False
-        self.bounds = bounds
+        self.bounds = bounds # (width, height)
+        self.wrap = wrap
 
     def is_dead(self):
         return self.dead
@@ -27,33 +32,30 @@ class Snake:
 
     def move(self, direction):
         r, c = self.head()
-        
         opposites = {'w': 's', 's': 'w', 'a':'d', 'd':'a'}
-        
         if self.current_dir and direction == opposites.get(self.current_dir):
             direction = self.current_dir
 
-        if direction == 's':
-            r += 1
-        elif direction == 'w':
-            r -= 1
-        elif direction == 'a':
-            c -= 1
-        elif direction == 'd':
-            c += 1
+        if direction == 's': r += 1
+        elif direction == 'w': r -= 1
+        elif direction == 'a': c -= 1
+        elif direction == 'd': c += 1
 
-        if (c < 0 or c > self.bounds[1]) or (r < 0 or r > self.bounds[0]):
-            self.dead = True
-            return
+        if self.wrap:
+            c = c % self.bounds[0]
+            r = r % self.bounds[1]
+        else:
+            if (c < 0 or c >= self.bounds[0]) or (r < 0 or r >= self.bounds[1]):
+                self.dead = True
+                return
         
         new_head = (r, c)
-        
-        if new_head in self._body:
+        check_body = self._body if self.grow_pending > 0 else self._body[:-1]
+        if new_head in check_body:
             self.dead = True
             return
 
         self.current_dir = direction
-        
         self._body.insert(0, new_head)
         if self.grow_pending > 0:
             self.grow_pending -= 1
