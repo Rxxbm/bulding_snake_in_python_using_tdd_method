5049f596bf6b5b6e73755a60a935b797d834fa32
Rubem <rubemcorrea0@gmail.com>
2026-05-03 00:47:30 -0300

    feat: implementa inicialização do Pygame e carregamento automático de assets

--- snake_screen.py
+++ snake_screen.py
@@ -1,69 +1,27 @@
+import pygame
 import os
-import sys
-import tty
-import termios
-import threading
-import time
-from game_engine import GameEngine
 
+class PygameScreen:
+    def __init__(self, bounds=(20, 20), cell_size=40):
+        self.bounds = bounds
+        self.cell_size = cell_size
+        self.width = bounds[0] * cell_size
+        self.height = bounds[1] * cell_size
+        self.display = pygame.display.set_mode((self.width, self.height))
+        self.assets = {}
+        self._load_assets()
+
+    def _load_assets(self):
+        graphics_path = "snake_graphics/Graphics"
+        if not os.path.exists(graphics_path): return
+        for file in os.listdir(graphics_path):
+            if file.endswith(".png"):
+                name = file.replace(".png", "")
+                img = pygame.image.load(os.path.join(graphics_path, file)).convert_alpha()
+                img = pygame.transform.scale(img, (self.cell_size, self.cell_size))
+                self.assets[name] = img
+
+# O código antigo do terminal continua aqui para ser refatorado depois
 class io_handler:
     def __init__(self, dim, speed):
-        self.x_size, self.y_size = dim
-        self.game_speed = speed
-        self.last_input = 'd'
-        self.matrix = [[0] * self.x_size for _ in range(self.y_size)]
-
-    def record_inputs(self):
-        def read_keys():
-            fd = sys.stdin.fileno()
-            old = termios.tcgetattr(fd)
-            try:
-                tty.setraw(fd)
-                while True:
-                    ch = sys.stdin.read(1)
-                    if ch in ('w', 'a', 's', 'd'): self.last_input = ch
-                    elif ch == '\x1b':
-                        self.last_input = 'end'
-                        break
-            finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old)
-        t = threading.Thread(target=read_keys, daemon=True)
-        t.start()
-
-    def sync_and_display(self, engine):
-        for r in range(self.y_size):
-            for c in range(self.x_size): self.matrix[r][c] = 0
-        for fr, fc in engine.fruits: self.matrix[fr][fc] = 3
-        for i, (r, c) in enumerate(engine.snake.body()):
-            if 0 <= r < self.y_size and 0 <= c < self.x_size:
-                self.matrix[r][c] = 2 if i == 0 else 1
-        output = "\033[H" + "+" + "--" * self.x_size + "+\r\n"
-        for line in self.matrix:
-            row = "|"
-            for item in line:
-                if item == 1: row += "[]"
-                elif item == 2: row += "<>"
-                elif item == 3: row += "()"
-                else: row += "  "
-            output += row + "|\r\n"
-        output += "+" + "--" * self.x_size + "+\r\n"
-        output += f"SCORE: {len(engine.snake.body())-1} | TECLA: {self.last_input.upper()}\r\n"
-        sys.stdout.write(output)
-        sys.stdout.flush()
-
-def main():
-    DIM = (20, 10)
-    engine = GameEngine(bounds=DIM)
-    io = io_handler(DIM, 0.15)
-    io.record_inputs()
-    sys.stdout.write("\033[2J\033[?25l")
-    try:
-        while not engine.snake.is_dead():
-            if io.last_input == 'end': break
-            engine.update(io.last_input)
-            io.sync_and_display(engine)
-            time.sleep(io.game_speed)
-    finally:
-        sys.stdout.write("\033[?25h\r\nGAME OVER\r\n")
-
-if __name__ == '__main__': main()
+        pass
